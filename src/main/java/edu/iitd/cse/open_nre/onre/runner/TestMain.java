5510a3c12d2acc4b6c3db763e38713869a7097e3
Harinder <sethi.harinder@gmail.com>
2016-02-21 16:20:42 +0530

    changing the package structure

---
+++ src/main/java/edu/iitd/cse/open_nre/onre/runner/TestMain.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package main.java.edu.iitd.cse.open_nre.onre.runner;
+
+import main.java.edu.iitd.cse.open_nre.onre.helper.MayIHelpYou;
+
+
+/**
+ * @author harinder
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("I am here");
+		MayIHelpYou.runMe();
+	}
+
+}
