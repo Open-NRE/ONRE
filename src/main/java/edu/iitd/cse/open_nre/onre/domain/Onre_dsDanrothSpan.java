09e32d49ea24fe8d690cd819ace39e41697fa2ba
Harinder <sethi.harinder@gmail.com>
2016-06-03 02:12:42 +0530

    forgot to add files...

---
+++ src/main/java/edu/iitd/cse/open_nre/onre/domain/Onre_dsDanrothSpan.java
@@ -0,0 +1,12 @@
+package edu.iitd.cse.open_nre.onre.domain;
+
+
+public class Onre_dsDanrothSpan {
+	public Double value;
+	public String phrase;
+	public String bound;
+	public String unit;
+	
+	public int start;
+	public int end;
+}
